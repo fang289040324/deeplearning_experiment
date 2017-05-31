cfb5b998e37aff68d437c6168332d4098949f571
fang <289040324@qq.com>
2017-05-31 21:04:42 +0800

    2.7

--- test_lstm_gru_keras.py
+++ test_lstm_gru_keras.py
@@ -11,12 +11,12 @@ x_data = np.linspace(0, 200, num=10000)
 y_data = np.cos(x_data) + 10
 # y_data = np.cos(x_data)
 
-print(x_data.shape, y_data.shape)
-print(x_data[0:10], y_data[10:20])
+print x_data.shape, y_data.shape
+print x_data[0:10], y_data[10:20]
 x_data = x_data.reshape([-1, 10, 1])
 y_data = y_data.reshape([-1, 10, 1])
-print(x_data[0:99].shape, y_data[1:100].shape)
-print(x_data[0], y_data[1])
+print x_data[0:99].shape, y_data[1:100].shape
+print x_data[0], y_data[1]
 
 batch_size = 3000
 lr = 1e-3
