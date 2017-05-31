e5c0017ab79d8826d3ca6c7ba57d7b1a2476130b
fang <289040324@qq.com>
2017-06-01 07:26:04 +0800

    0.2

--- train.py
+++ train.py
@@ -9,13 +9,17 @@ import tensorflow.contrib as tc
 import keras as k
 import sklearn.preprocessing as pre
 
-
-batch_size = 100
+batch_size = 500
 epochs = 10000
-validation_split = 0.1
-lr = 1e-3
+validation_split = 0.2
+lr = 1e-2
 seq_size = 60  # 50ms * 60 = 3s
 loss = k.losses.mae
+
+# 设置 GPU 按需增长
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
 
 
 def build_rnn(cell_type=None, isBidirectional=False, isBatchNorm=False, isNoise=False):
@@ -36,32 +40,57 @@ def build_rnn(cell_type=None, isBidirectional=False, isBatchNorm=False, isNoise=
         else:
             return layer
 
-    input_data = k.layers.Input((seq_size, x.shape[1]))
+    input_main_data = k.layers.Input((seq_size, x_main.shape[2]), name='input_main')
+    input_aux_data = k.layers.Input((seq_size, x_aux.shape[2]), name='input_aux')
     if isNoise:
-        gaussian_input_data = k.layers.GaussianNoise(0.1)(input_data)
-        cell = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True), isBidirectional)(gaussian_input_data), isBatchNorm)
+        gaussian_input_main_data = k.layers.GaussianNoise(0.5, name='gaussian_main')(input_main_data)
+        cell_main = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True, name='cell_main_1'), isBidirectional)(gaussian_input_main_data), isBatchNorm)
     else:
-        cell = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True), isBidirectional)(input_data), isBatchNorm)
-    cell = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True), isBidirectional)(cell), isBatchNorm)
+        cell_main = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True, name='cell_main_1'), isBidirectional)(input_main_data), isBatchNorm)
 
-    flatten = k.layers.Flatten()(cell)
-    encode_output = batchNorm(k.layers.Dense(512)(flatten), isBatchNorm)
-    encode_output = k.layers.RepeatVector(seq_size)(encode_output)
+    aux_output = batchNorm(k.layers.TimeDistributed(k.layers.Dense(1, name='aux_output'))(cell_main), isBatchNorm)
 
-    cell = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True), isBidirectional)(encode_output), isBatchNorm)
-    cell = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True), isBidirectional)(cell), isBatchNorm)
-    decode_output = k.layers.TimeDistributed(k.layers.Dense(1))(cell)
+    if isNoise:
+        gaussian_input_aux_data = k.layers.GaussianNoise(0.5)(input_aux_data)
+        cell_aux = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True, name='cell_aux_1'), isBidirectional)(gaussian_input_aux_data), isBatchNorm)
+    else:
+        cell_aux = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True, name='cell_aux_1'), isBidirectional)(input_aux_data), isBatchNorm)
 
-    model = k.models.Model(input_data, decode_output)
+    cell = k.layers.concatenate([cell_main, cell_aux], name='concatenate')
+    cell = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True, name='encode_layer'), isBidirectional)(cell), isBatchNorm)
+
+    flatten = k.layers.Flatten(name='flatten')(cell)
+    encode_output = batchNorm(k.layers.Dense(512, name='dnn_all_1')(flatten), isBatchNorm)
+    encode_output = k.layers.RepeatVector(seq_size, name='encode_output')(encode_output)
+
+    cell = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True, name='decode_layer_1'), isBidirectional)(encode_output), isBatchNorm)
+    cell = batchNorm(bidirectionalCell(rnn_cell(128, return_sequences=True, name='decode_layer_2'), isBidirectional)(cell), isBatchNorm)
+    decode_output = batchNorm(k.layers.TimeDistributed(k.layers.Dense(1, name='decode_output'))(cell), isBatchNorm)
+
+    model = k.models.Model([input_main_data, input_aux_data], [decode_output, aux_output])
     model.summary()
 
-    model.compile(optimizer=k.optimizers.Nadam(lr=lr), loss=loss, metrics=[k.metrics.mae])
+    model.compile(optimizer=k.optimizers.Adam(lr=lr), loss=loss, metrics=[k.metrics.mae], loss_weights=[1, 0.2])
     return model
 
 
+def train():
+    model = build_rnn(cell_type=None, isBatchNorm=True, isBidirectional=True, isNoise=False)
+    model.fit(x=[x_main[0: (x_main.shape[0] - 1)], x_aux[0: (x_aux.shape[0] - 1)]], y=[y[1: y.shape[0]], y[1: y.shape[0]]], batch_size=batch_size, epochs=epochs,
+              callbacks=[
+                  k.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.7, patience=30, verbose=1, cooldown=o),
+                  k.callbacks.ModelCheckpoint(filepath='./model/bmw/checkpoint/model-{epoch:02d}-{val_loss:.2f}.hdf5',
+                                              verbose=1, period=500, save_best_only=True, mode='min'),
+                  k.callbacks.TensorBoard(histogram_freq=50, write_graph=True, write_images=False)],
+              validation_split=validation_split)
+    model.save('./model/bmw/final_model.h5')
+
+
 def getData():
-    reader = csv.DictReader(open('C:/Users/fangsw/Desktop/BMW/BMWdata1206n.csv'))
-    x = []
+    # reader = csv.DictReader(open('/home/fang/PycharmProjects/experiment/data/bmw/BMWdata1206n.csv'))
+    reader = csv.DictReader(open('/home/fang/PycharmProjects/experiment/data/bmw/BMWdata1231_1.csv'))
+    x_main = []
+    x_aux = []
     y = []
 
     data_map = {}
@@ -148,40 +177,69 @@ def getData():
         result_datas.append(egoVehicle)
 
     for dmap in result_datas:
-        x.append(
-            [dmap['ExistenceProbability'], dmap['IndictatorLeft'], dmap['IndicatorRight'], dmap['TypeProbabilityCar'],
-             dmap['TypeProbabilityTruck'], dmap['TypeProbabilityBike'],
-             dmap['TypeProbabilityStationary'], dmap['ObjectWidth'], dmap['ObjectHeight'], dmap['ObjectLength'],
-             dmap['PositionRelX'], dmap['PositionRelY'], dmap['PositionRelZ'], dmap['PositionAbsX'],
-             dmap['PositionAbsY'], dmap['PositionAbsZ'], dmap['VelocityAbs'], dmap['VelocityRelX'],
-             dmap['VelocityRelY'], dmap['VelocityLateral'], dmap['AccelerationAbs'], dmap['AccelerationRel'],
-             dmap['YawAbs'], dmap['YawRel'], dmap['YawRelToLane'], dmap['LaneWidth'], dmap['LaneDistanceToLeftBorder'],
-             dmap['LaneDistanceToRightBorder'], dmap['LaneAngleToLeftBorder'], dmap['LaneAngleToRightBorder'],
-             dmap['LaneDistanceToEgo'], dmap['LaneLeftExists'], dmap['LaneRightExists'], dmap['drYawRelToLaneVal'],
-             dmap['dlYawRelToLaneVal'], dmap['sYawRelToLaneVal'],
-             dmap['drAccelerationAbsVal'], dmap['dlAccelerationAbsVal'], dmap['sAccelerationAbsVal'],
-             dmap['drVelocityAbsVal'], dmap['dlVelocityAbsVal'], dmap['sVelocityAbsVal'],
-             dmap['drLaneDistanceToEgoMin'],
-             dmap['dlLaneDistanceToEgoMin'], dmap['sLaneDistanceToEgoMin']])
+        # x.append(
+        #     [dmap['ExistenceProbability'], dmap['IndictatorLeft'], dmap['IndicatorRight'], dmap['TypeProbabilityCar'],
+        #      dmap['TypeProbabilityTruck'], dmap['TypeProbabilityBike'],
+        #      dmap['TypeProbabilityStationary'], dmap['ObjectWidth'], dmap['ObjectHeight'], dmap['ObjectLength'],
+        #      dmap['PositionRelX'], dmap['PositionRelY'], dmap['PositionRelZ'], dmap['PositionAbsX'],
+        #      dmap['PositionAbsY'], dmap['PositionAbsZ'], dmap['VelocityAbs'], dmap['VelocityRelX'],
+        #      dmap['VelocityRelY'], dmap['VelocityLateral'], dmap['AccelerationAbs'], dmap['AccelerationRel'],
+        #      dmap['YawAbs'], dmap['YawRel'], dmap['YawRelToLane'], dmap['LaneWidth'], dmap['LaneDistanceToLeftBorder'],
+        #      dmap['LaneDistanceToRightBorder'], dmap['LaneAngleToLeftBorder'], dmap['LaneAngleToRightBorder'],
+        #      dmap['LaneDistanceToEgo'], dmap['LaneLeftExists'], dmap['LaneRightExists'], dmap['drYawRelToLaneVal'],
+        #      dmap['dlYawRelToLaneVal'], dmap['sYawRelToLaneVal'],
+        #      dmap['drAccelerationAbsVal'], dmap['dlAccelerationAbsVal'], dmap['sAccelerationAbsVal'],
+        #      dmap['drVelocityAbsVal'], dmap['dlVelocityAbsVal'], dmap['sVelocityAbsVal'],
+        #      dmap['drLaneDistanceToEgoMin'],
+        #      dmap['dlLaneDistanceToEgoMin'], dmap['sLaneDistanceToEgoMin']])
+
+        x_main.append([dmap['VelocityAbs'], dmap['VelocityLateral'], dmap['AccelerationAbs'],
+                       dmap['YawAbs'], dmap['YawRelToLane'], dmap['LaneWidth'],
+                       dmap['LaneDistanceToLeftBorder'], dmap['LaneDistanceToRightBorder'],
+                       dmap['LaneAngleToLeftBorder'], dmap['LaneAngleToRightBorder'],
+                       dmap['LaneLeftExists'], dmap['LaneRightExists'],
+                       dmap['drYawRelToLaneVal'], dmap['dlYawRelToLaneVal'], dmap['sYawRelToLaneVal'],
+                       dmap['drAccelerationAbsVal'], dmap['dlAccelerationAbsVal'], dmap['sAccelerationAbsVal'],
+                       dmap['drVelocityAbsVal'], dmap['dlVelocityAbsVal'], dmap['sVelocityAbsVal'],
+                       dmap['drLaneDistanceToEgoMin'], dmap['dlLaneDistanceToEgoMin'], dmap['sLaneDistanceToEgoMin']])
+
+        x_aux.append([dmap['ExistenceProbability'], dmap['IndictatorLeft'], dmap['IndicatorRight'], dmap['TypeProbabilityCar'],
+                      dmap['TypeProbabilityTruck'], dmap['TypeProbabilityBike'], dmap['TypeProbabilityStationary'], dmap['ObjectWidth'],
+                      dmap['ObjectHeight'], dmap['ObjectLength'], dmap['PositionAbsX'], dmap['PositionAbsY'], dmap['PositionAbsZ'],
+                      dmap['VelocityRelX'], dmap['VelocityRelY']])
+        # dmap['PositionRelX'], dmap['PositionRelY'], dmap['PositionRelZ'], dmap['AccelerationRel'], dmap['YawRel'], dmap['LaneDistanceToEgo'],
+
         y.append(dmap['LaneDistanceToLeftBorder'])
 
-    return np.array(x)[0: (len(x) - len(x) % seq_size)].reshape((-1, seq_size, 45)), np.array(y)[0: (len(y) - len(y) % seq_size)].reshape((-1, seq_size, 1))
+    return np.array(x_main)[0: (len(x_main) - len(x_main) % seq_size)].reshape((-1, seq_size, len(x_main[0]))),\
+           np.array(x_aux)[0: (len(x_aux) - len(x_aux) % seq_size)].reshape((-1, seq_size, len(x_aux[0]))),\
+           np.array(y)[0: (len(y) - len(y) % seq_size)].reshape((-1, seq_size, 1))
 
 
-x, y = getData()
-print(x.shape)
-print(y.shape)
+if __name__ == '__main__':
+    # x_main, x_aux, y = getData()
+    #
+    # np.save('./data/bmw/x_main_all', x_main)
+    # np.save('./data/bmw/x_aux_all', x_aux)
+    # np.save('./data/bmw/y_all', y)
 
+    x_main = np.load('./data/bmw/x_main.npy')
+    x_aux = np.load('./data/bmw/x_aux.npy')
+    y = np.load('./data/bmw/y.npy')
 
-print(x[0: (x.shape[0] - 1)][2, 1])
-print(x[0: (x.shape[0] - 1)][2, 2])
-print(x[0: (x.shape[0] - 1)][2, 3])
-print(y[1: y.shape[0]][1, 1])
-print(y[1: y.shape[0]][1, 2])
-print(y[1: y.shape[0]][1, 3])
+    print(x_main.shape)
+    print(x_aux.shape)
+    print(y.shape)
 
+    print x_main[0:10]
+    print x_aux[0:10]
 
-def train():
-    model = build_rnn(cell_type='gru', isBatchNorm=True, isBidirectional=True, isNoise=True)
-    model.fit(x=x[0: (x.shape[0] - 1)], y=y[1: y.shape[0]], batch_size=batch_size, epochs=epochs,
-              callbacks=[], validation_split=validation_split)
+    print(x_main[0: (x_main.shape[0] - 1)][2, 21])
+    print(x_main[0: (x_main.shape[0] - 1)][2, 22])
+    print(x_main[0: (x_main.shape[0] - 1)][2, 23])
+    print(y[1: y.shape[0]][1, 21])
+    print(y[1: y.shape[0]][1, 22])
+    print(y[1: y.shape[0]][1, 23])
+
+    # build_rnn('gru', True, True, True)
+    train()
